70042104856402cc9e3f85f9c9ee9a52817b4f67
lcscout <lcscoutinho@outlook.com>
2022-01-10 00:11:58 -0300

    fix(player): only jump when on ground

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,7 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-	[SerializeField] private float _jumpForce = 30f;
+	[Header("Ground Settings")]
+	[Tooltip("A position marking where to check if the player is grounded")]
+	[SerializeField] private Transform _groundCheck;
+	[Tooltip("A mask determining what is ground to the character")]
+	[SerializeField] private LayerMask _groundLayerMask;
+
+	[Header("Movement Settings")]
+	[Space]
+	[Tooltip("Amount of force added when the player jumps")]
+	[SerializeField] private float _jumpForce = 10f;
+
+	private const float _GROUND_CHECK_RADIUS = .2f;
+
+	private bool _isGrounded = false;
 
 	private void OnEnable() {
 		InputsController.OnTouchInput += Jump;
@@ -17,7 +30,16 @@ public class PlayerController : MonoBehaviour {
 		// InputsController.OnDrag -= ;
 	}
 
+	private void FixedUpdate() {
+		OnGround();
+	}
+
 	private void Jump() {
-		GetComponent<Rigidbody>().AddForce(new Vector3(0, _jumpForce, 0) * Time.deltaTime, ForceMode.Impulse);
+		if (_isGrounded)
+			GetComponent<Rigidbody>().AddForce(new Vector3(0, _jumpForce, 0), ForceMode.VelocityChange);
+	}
+
+	private void OnGround() {
+		_isGrounded = Physics.CheckSphere(_groundCheck.position, _GROUND_CHECK_RADIUS, _groundLayerMask);
 	}
 }
