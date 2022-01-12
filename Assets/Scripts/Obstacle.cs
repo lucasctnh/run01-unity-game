5b3a99a1f87636c5365b1e2685634af3f98dab03
lcscout <lcscoutinho@outlook.com>
2022-01-12 19:23:29 -0300

    feat(obstacles): add object pooling

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,14 +7,18 @@ public class Obstacle : MonoBehaviour {
 	[Tooltip("The velocity which the obstacle will continuously move towards the player")]
 	[SerializeField] private float _moveLeftVelocity = 20f;
 
+	private Action<Obstacle> _killAction;
+
 	private void Update() {
 		transform.Translate(Vector3.left * _moveLeftVelocity * Time.deltaTime, Space.World);
 	}
+
+	public void SetKill(Action<Obstacle> killAction) => _killAction = killAction;
 
 	private void OnCollisionEnter(Collision other) {
 		if (other.gameObject.CompareTag("Player"))
 			Destroy(other.gameObject); // TODO: make gameover
 		if (other.gameObject.CompareTag("Obstacle Destroyer"))
-			Destroy(gameObject);
+			_killAction(this);
 	}
 }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class SpawnManager : MonoBehaviour {
 	[Tooltip("A list of every obstacle prefab to be randonly spawned")]
-	[SerializeField] private List<GameObject> _obstacles = new List<GameObject>();
+	[SerializeField] private List<Obstacle> _obstacles = new List<Obstacle>();
 
 	[Tooltip("A delay time in seconds to wait before start spawning obstacles")]
 	[SerializeField] private float _startDelay = 2;
@@ -12,13 +13,43 @@ public class SpawnManager : MonoBehaviour {
 	[Tooltip("The time in seconds in which the spawn will keep repeating")]
 	[SerializeField] private float _repeatRate = 2;
 
+	private ObjectPool<Obstacle> _pool;
+
 	private void Start() {
+		_pool = new ObjectPool<Obstacle>(CreateObstacle, OnGetObstacleFromPool, OnReleaseObstacleToPool,
+		obst => Destroy(obst.gameObject), true, 6, 12);
+
 		InvokeRepeating("SpawnObstacle", _startDelay, _repeatRate);
 	}
 
-	private void SpawnObstacle() {
-		int random = Random.Range(0, _obstacles.Count);
-		Vector3 spawnPosition = new Vector3(transform.position.x, _obstacles[random].transform.position.y, _obstacles[random].transform.position.z);
-        Instantiate(_obstacles[random], spawnPosition, _obstacles[random].transform.rotation);
+	private Obstacle CreateObstacle() {
+		int random = CreateRandomNumber();
+        return Instantiate(_obstacles[random], CreateRandomPosition(random), _obstacles[random].transform.rotation);
 	}
+
+	private int CreateRandomNumber() {
+		return Random.Range(0, _obstacles.Count);
+	}
+
+	private Vector3 CreateRandomPosition(int random) {
+		return new Vector3(transform.position.x, _obstacles[random].transform.position.y, _obstacles[random].transform.position.z);
+	}
+
+	private void OnGetObstacleFromPool(Obstacle obstacle) => obstacle.gameObject.SetActive(true);
+
+	private void OnReleaseObstacleToPool(Obstacle obstacle) {
+		obstacle.transform.position = ResetPosition(obstacle);
+		obstacle.gameObject.SetActive(false);
+	}
+
+	private Vector3 ResetPosition(Obstacle obstacle) {
+		return new Vector3(transform.position.x, obstacle.transform.position.y, obstacle.transform.position.z);
+	}
+
+	private void SpawnObstacle() {
+		Obstacle obstacle = _pool.Get();
+		obstacle.GetComponent<Obstacle>().SetKill(Kill);
+	}
+
+	private void Kill(Obstacle obstacle) => _pool.Release(obstacle);
 }
