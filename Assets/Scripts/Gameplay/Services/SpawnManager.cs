2083c360edd5a600e9a83934988b970762830b2e
lcscout <lcscoutinho@outlook.com>
2022-02-05 14:17:00 -0300

    refactor(SpawnManager): now supports multiple prefabs

--- Assets/Scripts/Gameplay/Services/SpawnManager.cs
+++ Assets/Scripts/Gameplay/Services/SpawnManager.cs
@@ -3,10 +3,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Pool;
+using Random = UnityEngine.Random;
 
 public class SpawnManager : MonoBehaviour {
 	[Tooltip("The hitable prefab")]
-	[SerializeField] private Hitable _hitablePrefab;
+	[SerializeField] private Hitable[] _hitablePrefabs;
 
 	[Tooltip("A delay time in seconds to wait before start spawning collectables")]
 	[SerializeField] private float _startDelay = 2;
@@ -14,12 +15,13 @@ public class SpawnManager : MonoBehaviour {
 	[Tooltip("The time in seconds in which the spawn will keep repeating")]
 	[SerializeField] private float _repeatRate = 2;
 
-	private ObjectPool<Hitable> _pool;
+	private List<ObjectPool<Hitable>> _pools = new List<ObjectPool<Hitable>>();
 	private float _timer = 0f;
 
 	private void Start() {
-		_pool = new ObjectPool<Hitable>(CreateHitable, OnGetHitableFromPool, OnReleaseHitableToPool,
-		hitable => Destroy(hitable.gameObject), true, 6, 12);
+		foreach (Hitable hitablePrefab in _hitablePrefabs)
+			_pools.Add(new ObjectPool<Hitable>(() => CreateHitable(hitablePrefab), OnGetHitableFromPool, OnReleaseHitableToPool,
+				hitable => Destroy(hitable.gameObject), true, 6, 12));
 
 		_timer = _startDelay;
 	}
@@ -29,25 +31,30 @@ public class SpawnManager : MonoBehaviour {
 			GameManager.CallRepeating(SpawnHitable, ref _timer, _repeatRate);
 	}
 
-	private Hitable CreateHitable() {
-		return Instantiate(_hitablePrefab, CreateRandomPosition(_hitablePrefab), _hitablePrefab.transform.rotation);
+	private Hitable CreateHitable(Hitable hitablePrefab) {
+		return Instantiate(hitablePrefab, GenerateRandomPosition(hitablePrefab), hitablePrefab.transform.rotation);
 	}
 
-	private Vector3 CreateRandomPosition(Hitable hitable) {
-		return hitable.CreateRandomPosition();
+	private Vector3 GenerateRandomPosition(Hitable hitable) {
+		return hitable.GenerateRandomPosition(transform.position.x);
 	}
 
 	private void OnGetHitableFromPool(Hitable hitable) => hitable.gameObject.SetActive(true);
 
 	private void OnReleaseHitableToPool(Hitable hitable) {
-		hitable.transform.position = CreateRandomPosition(hitable);
+		hitable.transform.position = GenerateRandomPosition(hitable);
 		hitable.gameObject.SetActive(false);
 	}
 
 	private void SpawnHitable() {
-		Hitable hitable = _pool.Get();
-		hitable.GetComponent<Hitable>().SetKill(Kill);
+		ObjectPool<Hitable> pool = GetRandomPool();
+		Hitable hitable = pool.Get();
+		hitable.GetComponent<Hitable>().SetKill(hitable => Kill(pool, hitable));
 	}
 
-	private void Kill(Hitable hitable) => _pool.Release(hitable);
+	private ObjectPool<Hitable> GetRandomPool() {
+		return _pools[Random.Range(0, _pools.Count)];
+	}
+
+	private void Kill(ObjectPool<Hitable> pool, Hitable hitable) => pool.Release(hitable);
 }
